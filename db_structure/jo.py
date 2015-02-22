e7445deea32791fff91376c46a10df0c2e67e88a
Leonidas Christodoulou <leo@Leonidass-MacBook-Pro.local>
2015-02-22 15:01:04 +0000

    run_db.py added. Now each component seems to run fine individually.

--- db_structure/jo.py
+++ db_structure/jo.py
@@ -1,12 +1,12 @@
 import sys
-#import db_utilities
+import db_utilities
 
 def hello_world():
     print("Hello world!")
     return
 
 def main():
-    sys.exit(hello_world())
+    hello_world()
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
